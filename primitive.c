66bd5a4b8a269d77f2a75c12c1d93071d33185f0
Jim Ingram <ingramj@gmail.com>
2010-01-16 15:30:56 -0600

    Added a subtraction primitive

--- primitive.c
+++ primitive.c
@@ -5,8 +5,10 @@
  */
 
 #include "primitive.h"
+#include "error.h"
 #include "object.h"
 #include "eval.h"
+
 
 object *add_proc(object *arguments)
 {
@@ -20,10 +22,34 @@ object *add_proc(object *arguments)
 }
 
 
+object *sub_proc(object *arguments)
+{
+    if (is_empty_list(arguments)) {
+        error("- requires at least one argument");
+    }
+
+    long result = car(arguments)->value.number;
+    arguments = cdr(arguments);
+    if (is_empty_list(arguments)) {
+        return make_number(-result);
+    }
+
+    while (!is_empty_list(arguments)) {
+        result -= (car(arguments))->value.number;
+        arguments = cdr(arguments);
+    }
+    return make_number(result);
+}
+
+
 void init_primitives(void)
 {
     define_variable(make_symbol("+"),
             make_primitive(add_proc),
+            get_global_environment());
+
+    define_variable(make_symbol("-"),
+            make_primitive(sub_proc),
             get_global_environment());
 }
 
