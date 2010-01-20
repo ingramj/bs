a23126aed29987474e3b173d2806d3baac32fb95
Jim Ingram <ingramj@gmail.com>
2010-01-19 20:45:05 -0600

    Reorganization and cleanup of eval.c

--- eval.c
+++ eval.c
@@ -1,4 +1,4 @@
-/* Expression evaluator for a Scheme-like language.
+/* Expression evaluator.
  *
  * Copyright (c) 2010 James E. Ingram
  * See the LICENSE file for terms of use.
@@ -11,56 +11,59 @@
 #include "error.h"
 
 
-static int is_self_evaluating(object *obj);
-static inline int is_variable(object *obj);
-static int is_tagged_list(object *obj, object *tag);
-static inline int is_quoted(object *obj);
-
-/**** Assignment expressions ****/
+/**** Identification ****/
+static inline int is_self_evaluating(object *exp);
+static inline int is_tagged_list(object *exp, object *tag);
+static inline int is_variable(object *exp);
+static inline int is_quoted(object *exp);
 static inline int is_assignment(object *exp);
+static inline int is_definition(object *exp);
+static inline int is_if(object *exp);
+static inline int is_begin(object *exp);
+static inline int is_lambda(object *exp);
+static inline int is_application(object *exp);
+
+
+/**** Decomposition ****/
+static inline object *quoted_expression(object *exp);
 static inline object *assignment_variable(object *exp);
 static inline object *assignment_value(object *exp);
-static object *eval_assignment(object *exp, object *env);
-
-/**** Definition expressions ****/
-static inline int is_definition(object *exp);
 static inline object *definition_variable(object *exp);
 static inline object *definition_value(object *exp);
-static object *eval_definition(object *exp, object *env);
-
-/**** Conditional expressions ****/
-static inline int is_if(object *exp);
 static inline object *if_predicate(object *exp);
 static inline object *if_consequent(object *exp);
 static inline object *if_alternate(object *exp);
-
-/**** Begin expressions ****/
-static inline int is_begin(object *exp);
-static inline object *make_begin(object *exp);
 static inline object *begin_actions(object *exp);
-
-/**** Lambda expressions ****/
-static inline int is_lambda(object *exp);
-static inline object *make_lambda(object *parameters, object *body);
 static inline object *lambda_parameters(object *exp);
 static inline object *lambda_body(object *exp);
-
-/**** Procedure applications ****/
-static inline int is_application(object *exp);
-static inline object *operator(object *exp);
-static inline object *operands(object *exp);
-static object *eval_argument_list(object *args, object *env);
+static inline object *application_operator(object *exp);
+static inline object *application_operands(object *exp);
 
 
-static int is_self_evaluating(object *obj)
+/**** Conversion ****/
+static inline object *make_begin(object *exp);
+static inline object *make_lambda(object *parameters, object *body);
+
+
+/**** Evaluation ****/
+static object *eval_assignment(object *exp, object *env);
+static object *eval_definition(object *exp, object *env);
+static object *eval_parameters(object *parameters, object *env);
+
+
+/**** Identification ****/
+static inline int is_self_evaluating(object *exp)
 {
-    if (obj == NULL) {
-        error("null object");
-    }
+    object_type t = exp->type;
+    return (t == NUMBER || t == BOOLEAN || t == CHARACTER || t == STRING) ?
+        1 : 0;
+}
 
-    if (obj->type == NUMBER || obj->type == BOOLEAN ||
-            obj->type == CHARACTER || obj->type == STRING) {
-        return 1;
+
+static inline int is_tagged_list(object *exp, object *tag)
+{
+    if (is_list(exp)) {
+        return is_symbol(car(exp)) && car(exp) == tag;
     } else {
         return 0;
     }
@@ -79,20 +82,47 @@ static inline int is_quoted(object *exp)
 }
 
 
-static int is_tagged_list(object *exp, object *tag)
-{
-    if (is_pair(exp)) {
-        object *car_obj = car(exp);
-        return is_symbol(car_obj) && (car_obj == tag);
-    }
-    return 0;
-}
-
-
-/**** Assignment expressions ****/
 static inline int is_assignment(object *exp)
 {
     return is_tagged_list(exp, lookup_symbol("set!"));
+}
+
+
+static inline int is_definition(object *exp)
+{
+
+    return is_tagged_list(exp, lookup_symbol("define"));
+}
+
+
+static inline int is_if(object *exp)
+{
+    return is_tagged_list(exp, lookup_symbol("if"));
+}
+
+
+static inline int is_begin(object *exp)
+{
+    return is_tagged_list(exp, lookup_symbol("begin"));
+}
+
+
+static inline int is_lambda(object *exp)
+{
+    return is_tagged_list(exp, lookup_symbol("lambda"));
+}
+
+
+static inline int is_application(object *exp)
+{
+    return is_list(exp);
+}
+
+
+/**** Decomposition ****/
+static inline object *quoted_expression(object *exp)
+{
+    return car(cdr(exp));
 }
 
 
@@ -108,55 +138,16 @@ static inline object *assignment_value(object *exp)
 }
 
 
-static object *eval_assignment(object *exp, object *env)
-{
-    set_variable_value(assignment_variable(exp),
-            bs_eval(assignment_value(exp), env),
-            env);
-    return lookup_symbol("ok");
-}
-
-
-/**** Definition expressions ****/
-static inline int is_definition(object *exp)
-{
-    return is_tagged_list(exp, lookup_symbol("define"));
-}
-
-
 static inline object *definition_variable(object *exp)
 {
-    if (is_symbol(car(cdr(exp)))) {
-        return car(cdr(exp));
-    } else {
-        return car(car(cdr(exp)));
-    }
+    return is_symbol(car(cdr(exp))) ? car(cdr(exp)) : car(car(cdr(exp)));
 }
 
 
 static inline object *definition_value(object *exp)
 {
-    if (is_symbol(car(cdr(exp)))) {
-        return car(cdr(cdr(exp)));
-    } else {
-        return make_lambda(cdr(car(cdr(exp))), cdr(cdr(exp)));
-    }
-}
-
-
-static object *eval_definition(object *exp, object *env)
-{
-    define_variable(definition_variable(exp),
-            bs_eval(definition_value(exp), env),
-            env);
-    return lookup_symbol("ok");
-}
-
-
-/**** Conditional expressions ****/
-static inline int is_if(object *exp)
-{
-    return is_tagged_list(exp, lookup_symbol("if"));
+    return is_symbol(car(cdr(exp))) ? car(cdr(cdr(exp))) :
+        make_lambda(cdr(car(cdr(exp))), cdr(cdr(exp)));
 }
 
 
@@ -174,45 +165,18 @@ static inline object *if_consequent(object *exp)
 
 static inline object *if_alternate(object *exp)
 {
-    object *alt_branch = cdr(cdr(cdr(exp)));
-    if (is_empty_list(alt_branch)) {
-        // What to return in this situation is unspecified by the standard.
+    object *alt = cdr(cdr(cdr(exp)));
+    if (is_empty_list(alt)) {
         return get_boolean(0);
     } else {
-        return car(alt_branch);
+        return car(alt);
     }
-}
-
-
-/**** Begin expressions ****/
-static inline int is_begin(object *exp)
-{
-    return is_tagged_list(exp, lookup_symbol("begin"));
-}
-
-
-static inline object *make_begin(object *exp)
-{
-    return cons(lookup_symbol("begin"), exp);
 }
 
 
 static inline object *begin_actions(object *exp)
 {
     return cdr(exp);
-}
-
-
-/**** Lambda expressions ****/
-static inline int is_lambda(object *exp)
-{
-    return is_tagged_list(exp, lookup_symbol("lambda"));
-}
-
-
-static inline object *make_lambda(object *parameters, object *body)
-{
-    return cons(lookup_symbol("lambda"), cons(parameters, body));
 }
 
 
@@ -228,68 +192,79 @@ static inline object *lambda_body(object *exp)
 }
 
 
-/**** Procedure applications ****/
-static inline int is_application(object *exp)
-{
-    return is_pair(exp);
-}
-
-
-static inline object *operator(object *exp)
+static inline object *application_operator(object *exp)
 {
     return car(exp);
 }
 
 
-static inline object *operands(object *exp)
+static inline object *application_operands(object *exp)
 {
     return cdr(exp);
 }
 
 
-static object *eval_argument_list(object *args, object *env)
+/**** Conversion ****/
+static inline object *make_begin(object *exp)
 {
-    if (is_empty_list(args)) {
-        return get_empty_list();
-    } else {
-        return cons(bs_eval(car(args), env),
-                eval_argument_list(cdr(args), env));
-    }
+    return cons(lookup_symbol("begin"), exp);
 }
 
 
-/**** Scheme's eval procedure. ****/
-void init_special_forms(void)
+static inline object *make_lambda(object *parameters, object *body)
 {
-    make_symbol("quote");
-    make_symbol("set!");
-    make_symbol("define");
-    make_symbol("ok");  // not a special form, but returned by define and set!
-    make_symbol("if");
-    make_symbol("begin");
-    make_symbol("lambda");
+    return cons(lookup_symbol("lambda"), cons(parameters, body));
+}
+
+
+/**** Evaluation ****/
+static object *eval_assignment(object *exp, object *env)
+{
+    set_variable_value(assignment_variable(exp),
+            bs_eval(assignment_value(exp), env),
+            env);
+    return lookup_symbol("ok");
+}
+
+
+static object *eval_definition(object *exp, object *env)
+{
+    define_variable(definition_variable(exp),
+            bs_eval(definition_value(exp), env),
+            env);
+    return lookup_symbol("ok");
+}
+
+
+static object *eval_parameters(object *parameters, object *env)
+{
+    if (is_empty_list(parameters)) {
+        return get_empty_list();
+    } else {
+        return cons(bs_eval(car(parameters), env),
+                eval_parameters(cdr(parameters), env));
+    }
 }
 
 
 object *bs_eval(object *exp, object *env)
 {
-
 tailcall:
     if (is_self_evaluating(exp)) {
         return exp;
     } else if (is_variable(exp)) {
         return lookup_variable_value(exp, env);
     } else if (is_quoted(exp)) {
-        return car(cdr(exp));
+        return quoted_expression(exp);
     } else if (is_assignment(exp)) {
         return eval_assignment(exp, env);
     } else if (is_definition(exp)) {
         return eval_definition(exp, env);
     } else if (is_if(exp)) {
-        if (is_false(bs_eval(if_predicate(exp), env))) {
-            exp = if_alternate(exp);
-        } else {
+        if (!is_false(bs_eval(if_predicate(exp), env))) {
             exp = if_consequent(exp);
+        } else {
+            exp = if_alternate(exp);
         }
         goto tailcall;
     } else if (is_lambda(exp)) {
@@ -298,9 +273,7 @@ tailcall:
                 env);
     } else if (is_begin(exp)) {
         exp = begin_actions(exp);
-        if (is_empty_list(exp)) {
-            error("empty begin block");
-        }
+        if (is_empty_list(exp)) { error("empty begin block"); }
         while (!is_empty_list(cdr(exp))) {
             bs_eval(car(exp), env);
             exp = cdr(exp);
@@ -308,22 +281,34 @@ tailcall:
         exp = car(exp);
         goto tailcall;
     } else if (is_application(exp)) {
-        object *procedure = bs_eval(operator(exp), env);
-        object *arguments = eval_argument_list(operands(exp), env);
+        object *procedure = bs_eval(application_operator(exp), env);
+        object *parameters = eval_parameters(application_operands(exp), env);
         if (is_primitive_proc(procedure)) {
-            return (procedure->value.primitive_proc)(arguments);
+            return (procedure->value.primitive_proc)(parameters);
         } else if (is_compound_proc(procedure)) {
             env = extend_environment(
                     procedure->value.compound_proc.parameters,
-                    arguments,
+                    parameters,
                     procedure->value.compound_proc.env);
             exp = make_begin(procedure->value.compound_proc.body);
             goto tailcall;
         } else {
-            error("unknown procedure type");
+            error("unable to apply unknown procedure type");
         }
     } else {
         error("unable to evaluate expression");
     }
+}
+
+
+void init_special_forms(void)
+{
+    make_symbol("quote");
+    make_symbol("set!");
+    make_symbol("define");
+    make_symbol("ok");
+    make_symbol("if");
+    make_symbol("begin");
+    make_symbol("lambda");
 }
 
